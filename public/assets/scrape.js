effcc68c6d47485ebdf4db5ab15afb46a6f4825f
Ying Zhou <yzhouyzhou@yahoo.com>
2019-08-27 11:57:16 -0500

    the first working drafts

--- public/assets/scrape.js
+++ public/assets/scrape.js
@@ -1,3 +1,4 @@
+let isNewNote = true;
 
 $(document).on("click", "#delete-comment", function () {
     var thisNote = $(this).attr("data-id");
@@ -7,13 +8,14 @@ $(document).on("click", "#delete-comment", function () {
     // });
     $.ajax({
         method: "DELETE",
-        url: "note",
+        url: "note/" + thisNote,
         data: {
-            _id: thisNote
-          }     
-    }).then(function(data){
+            noteid: thisNote
+        }
+    }).then(function (data) {
         location.reload();
     })
+    location.reload();
 })
 
 $(document).on("click", "#add-comment", function () {
@@ -28,9 +30,6 @@ $(document).on("click", "#add-comment", function () {
         .then(function (data) {
             console.log(data);
             $('#myModal').modal("show");
-           var btn=$("#savenote");
-           btn.attr("data-id", data._id);
-           console.log("thisId 2", data._id);
 
             // If there's a note in the article
             if (data.note) {
@@ -38,46 +37,101 @@ $(document).on("click", "#add-comment", function () {
                 $("#titleinput").val(data.note.title);
                 // Place the body of the note in the body textarea
                 $("#bodyinput").val(data.note.body);
-                $("#notes").append("<h4>" + data.note.title + "</br>" + data.note.body + "</h4>");
+                var btnUpdate = $("#savenote");
+                btnUpdate.attr("data-id", data.note._id);
+                isNewNote = false;
+                console.log("thisId for update note", data.note._id);
             }
+            else {
+                isNewNote = true;
+                var btnNew = $("#savenote");
+                btnNew.attr("data-id", data._id);
+                console.log("thisId for create new", data._id);
+            }
+
         });
-
 });
-
-
 
 $(document).on("click", "#savenote", function () {
     // Grab the id associated with the article from the submit button
     var thisId = $(this).attr("data-id");
-    console.log("savenote" , thisId);    
-  
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
-      }
-    })
-      // With that done
-      .then(function (data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        // $("#notes").empty();
-        location.reload();
-       
-        
-      });
-  
+    console.log("savenote", thisId);
+
+    if (isNewNote) {
+        // Run a POST request to create a new note, using what's entered in the inputs
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+                // Value taken from title input
+                title: $("#titleinput").val(),
+                // Value taken from note textarea
+                body: $("#bodyinput").val()
+            }
+        })
+            // With that done
+            .then(function (data) {
+                // Log the response
+                console.log(data);
+                location.reload();
+            });
+    }
+    else {
+        // Run a PUT request to update the note, using what's entered in the inputs
+        $.ajax({
+            method: "PUT",
+            url: "/note/" + thisId,
+            data: {
+                // Value taken from title input
+                title: $("#titleinput").val(),
+                // Value taken from note textarea
+                body: $("#bodyinput").val()
+            }
+        })
+            // With that done
+            .then(function (data) {
+                // Log the response
+                console.log(data);
+                location.reload();
+            });
+    }
     // Also, remove the values entered in the input and textarea for note entry
     $("#titleinput").val("");
     $("#bodyinput").val("");
-    
-  });
+    location.reload();
+});
+
+
+
+
+// $(document).on("click", "#newnote", function () {
+//     // Grab the id associated with the article from the submit button
+//     var thisId = $(this).attr("data-id");
+//     console.log("newnote", thisId);
+
+//     // Run a POST request to change the note, using what's entered in the inputs
+//     $.ajax({
+//         method: "POST",
+//         url: "/articles/" + thisId,
+//         data: {
+//             // Value taken from title input
+//             title: $("#titleinput").val(),
+//             // Value taken from note textarea
+//             body: $("#bodyinput").val()
+//         }
+//     })
+//         // With that done
+//         .then(function (data) {
+//             // Log the response
+//             console.log(data);
+//             location.reload();
+//         });
+
+//     // Also, remove the values entered in the input and textarea for note entry
+//     $("#titleinput").val("");
+//     $("#bodyinput").val("");
+
+// });
 
 
 var handleScrape = () => {
@@ -89,6 +143,7 @@ var handleClear = () => {
     $.get("/clear").then(function (data) {
         location.reload();
     });
+    location.reload();
 }
 $("#scrape").on("click", handleScrape);
 $("#clear").on("click", handleClear);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,18 @@ var cheerio = require("cheerio");
 var db = require("../models");
 
 module.exports = function (app) {
+    app.get("/", function (req, res) {
+        db.Article.find({}).sort({ _id: -1 })
+            .populate("note")
+            .then(dbArticles => {
+                console.log(dbArticles);
+                res.render("index", { dbArticles });
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
     app.get("/clear", function (req, res) {
         // Grab every document in the Articles collection
         db.Note.deleteMany({})
@@ -17,6 +29,7 @@ module.exports = function (app) {
             });
         res.send("All articles with notes are cleared");
     });
+
     app.get("/scrape", function (req, res) {
         let count = 0;
         axios.get("https://abcnews.go.com").then(function (response) {
@@ -56,12 +69,8 @@ module.exports = function (app) {
 
             // Send a message to the client
             res.send(count + " News Scraped Complete");
-
         });
-
     });
-
-
 
     // Route for getting all Articles from the db
     app.get("/articles", function (req, res) {
@@ -77,17 +86,6 @@ module.exports = function (app) {
             });
     });
 
-    app.get("/", function (req, res) {
-        db.Article.find({}).sort({ _id: -1 })
-            .populate("note")
-            .then(dbArticles => {
-                console.log(dbArticles);
-                res.render("index", { dbArticles });
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
 
     // Route for grabbing a specific Article by id, populate it with it's note
     app.get("/articles/:id", function (req, res) {
@@ -105,7 +103,7 @@ module.exports = function (app) {
             });
     });
 
-    // Route for saving/updating an Article's associated Note
+    // Route for creating an Article's associated Note
     app.post("/articles/:id", function (req, res) {
         // Create a new note and pass the req.body to the entry
         db.Note.create(req.body)
@@ -125,20 +123,48 @@ module.exports = function (app) {
             });
     });
 
-    app.delete("/note", function (req, res) {
-        console.log("hey, here");
-        db.Note.deleteOne({ _id: req.params.commentid }).then(dbNote => {
-            db.Article.findOne({ note: req.params.commentid }, function (err, dbArticle) {
+    // Route for updating an Article's associated Note
+    app.put("/note/:noteid", function (req, res) {
+        // Create a new note and pass the req.body to the entry
+        db.Note.findOneAndUpdate({ _id: req.params.noteid },
+            {
+                title: req.body.title,
+                body: req.body.body
+            }
+        ).then(dbNote => {
+            res.json(dbNote);
+        });
+        // db.Note.create(req.body)
+        //     .then(function (dbNote) {
+        //         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        //         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        //         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        //         return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        //     })
+        //     .then(function (dbArticle) {
+        //         // If we were able to successfully update an Article, send it back to the client
+        //         res.json(dbArticle);
+        //     })
+        //     .catch(function (err) {
+        //         // If an error occurred, send it to the client
+        //         res.json(err);
+        //     });
+    });
+
+
+    app.delete("/note/:noteid", function (req, res) {
+        console.log("hey, here", req.params.noteid);
+        db.Note.deleteOne({ _id: req.params.noteid }).then(dbNote => {
+            console.log("Note is deleted now checking its Article...");
+            db.Article.findOne({ note: req.params.noteid }, function (err, dbArticle) {
                 if (dbArticle != null) {
-                    console.log(dbArticle) //output all props
-                    delete dbArticle.note || delete dbArticle['note']
-                    console.log(dbArticle) //output all props
+                    db.Article.update({ note: req.params.noteid },
+                        { $unset: { note: "" } }, function (err, newdbArticle) {
+                            console.log("Unset note property from article property ok ", newdbArticle); //output all props
+                        }
+                    )
                 }
             });
         });
     });
-
-
-
-
 };
